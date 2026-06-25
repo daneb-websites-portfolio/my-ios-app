f6cd7ba1c88827e4b3b7481c74554ab70d0430f7
daneb-websites-portfolio <danebwebsites@gmail.com>
2026-06-25 19:03:00 +0100

    Create App.swift

---
+++ App.swift
@@ -0,0 +1,52 @@
+import Frameworks
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let config = WKWebViewConfiguration()
+        
+        // Inject JavaScript to completely block window.open popups
+        let js = "window.open = function() { return null; };"
+        let script = WKUserScript(source: js, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        config.userContentController.addUserScript(script)
+        
+        webView = WKWebView(frame: self.view.bounds, configuration: config)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.view.addSubview(webView)
+        
+        if let url = URL(string: "https://netplayz.top") {
+            webView.load(URLRequest(url: url))
+        }
+    }
+    
+    // Strict Sandbox: Block any navigation away from your main website domain
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url, let host = url.host {
+            if host.contains("netplayz.top") {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        decisionHandler(.cancel) // Silently drops third-party ad network domains
+    }
+}
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
